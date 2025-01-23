7a149fc90a3f89bdc017020d6b8bd1bae943cf9f
Jeet Gaikwad <jeetgaikwad95@gmail.com>
2025-01-23 12:14:37 +0530

    feat:added the point system in the rock-paper-scissor game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,50 +4,79 @@ import random
 def get_computer_choice():
     return random.randint(1, 3)
 
-def determine_winner(user_choice, computer_choice):
+
+def determine_winner(user_choice, computer_choice, user_point, computer_point):
+    """
+        Based on choice, points are given to the user or computer.
+    """
     if user_choice == computer_choice:
         print("It's a tie")
     elif user_choice == "Rock" and computer_choice == "Paper":
         print("Computer wins! Paper covers Rock")
+        computer_point += 1
     elif user_choice == "Paper" and computer_choice == "Rock":
         print("User wins! Paper covers Rock")
+        user_point += 1
     elif user_choice == "Rock" and computer_choice == "Scissor":
         print("User wins! Rock crushes Scissor")
+        user_point += 1
     elif user_choice == "Scissor" and computer_choice == "Rock":
         print("Computer wins! Rock crushes Scissor")
+        computer_point += 1
     elif user_choice == "Paper" and computer_choice == "Scissor":
         print("Computer wins! Scissor cuts Paper")
+        computer_point += 1
     elif user_choice == "Scissor" and computer_choice == "Paper":
         print("User wins! Scissor cuts Paper")
-    
+        user_point += 1
+
+    return user_point, computer_point
+
 
 def main():
+    """
+        Main function of the Rock, Paper, Scissor game.
+    """
     options = {
         1: "Rock",
         2: "Paper",
         3: "Scissor"
     }
-    
-    print("Welcome to the Rock, Paper, and Scissor game.")
-    for index, value in options.items():
-        print(f"{index}. {value}")
+    user_points = 0
+    computer_points = 0
 
-    try:
-        user_input = int(input("Enter your choice (1-3): "))
-        if user_input not in options:
-            print("Invalid choice! Please select a valid option.")
-            
-        user_choice = options[user_input]
-        computer_choice = options[get_computer_choice()]
-        
-        print(f"\n You chose: {user_choice}")
-        print(f"Computer chose: {computer_choice}\n")
-        
-        result = determine_winner(user_choice, computer_choice)
-        
-        print(result)
-    except ValueError:
-        print("Invalid input! Please enter a number from 1 to 3.")
+    print("Welcome to the Rock, Paper, and Scissor game.")
+
+    while True:
+
+        for index, value in options.items():
+            print(f"{index}. {value}")
+
+        try:
+            user_input = int(input("Enter your choice (1-3): "))
+            if user_input not in options:
+                print("Invalid choice! Please select a valid option.")
+                continue
+
+            user_choice = options[user_input]
+            computer_choice = options[get_computer_choice()]
+
+            print(f"\n You chose: {user_choice}")
+            print(f"Computer chose: {computer_choice}\n")
+
+            user_points, computer_points = determine_winner(
+                user_choice, computer_choice, user_points, computer_points)
+
+            if user_points == 5:
+                print(f"User wins the game by {user_points}-{computer_points}!")
+                break
+            elif computer_points == 5:
+                print(f"Computer wins the game by {computer_points}-{user_points}!")
+                break
+
+            # print(result)
+        except ValueError:
+            print("Invalid input! Please enter a number from 1 to 3.")
 
 
 main()
